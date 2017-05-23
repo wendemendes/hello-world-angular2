0ab24a48d34716e93554214dbfa218eda0668fd6
Wende Mendes <wende@bluesoft.com.br>
2017-05-23 20:06:20 -0300

    hello world com angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro',
+  templateUrl: './meu-primeiro.component.html',
+  styleUrls: ['./meu-primeiro.component.css']
+})
+export class MeuPrimeiroComponent {
+
+  
+
+}
